1f5c76fb87bb066c232be137ea4ab0887167b765
ayoolaowoilu <ayoolaowoilu@gmail.com>
2026-06-05 22:28:04 +0100

    New : Created the client side of this application

--- server/data.sql
+++ server/data.sql
@@ -1,10 +1,22 @@
 create table users(
   id int auto_increment primary key ,
-  email varchar(255),
-  password varchar(255),
-  name text,
+  email varchar(255) not null,
+  password varchar(255) not null,
+  name text not null,
     role enum('admin','user') default 'user',
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp on update current_timestamp
-  
+   
+ )
+
+ create table secret_keys(
+   id int auto_increment primary key , 
+   user_id int not null ,
+   plan enum('normal','pro','pro_plus') default 'normal',
+   region varchar(255) not null,
+   name text not null,
+   _key text,
+   is_active int default 0,
+   created_at timestamp default current_timestamp,
+   updated_at timestamp default current_timestamp on update current_timestamp
  )
